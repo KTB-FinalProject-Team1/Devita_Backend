3364a0d06daff0f7f4ad8e0bd5869cfba600819b
alswp006 <alswp006@github.com>
2024-10-20 22:18:07 +0900

    first commit

---
+++ src/main/java/com/devita/DevitaApplication.java
@@ -0,0 +1,13 @@
+package com.devita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevitaApplication.class, args);
+	}
+
+}
