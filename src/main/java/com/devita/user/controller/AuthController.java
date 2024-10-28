f3a023e3baaa57132997dda7353f76f2c6b4bcbc
alswp006 <alswp006@github.com>
2024-10-28 10:46:43 +0900

    auth restcontroller 적용, cors 허용

--- src/main/java/com/devita/user/controller/AuthController.java
+++ src/main/java/com/devita/user/controller/AuthController.java
@@ -4,39 +4,40 @@ import com.devita.common.exception.ErrorCode;
 import com.devita.common.exception.SecurityTokenException;
 import com.devita.common.response.ApiResponse;
 import com.devita.user.jwt.JwtTokenProvider;
-import com.devita.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @Slf4j
-@RequestMapping("/api")
+@RequestMapping("/api/v1/auth")
 public class AuthController {
 
-    private final UserService userService;
     private final JwtTokenProvider jwtTokenProvider;
 
 
-    @GetMapping("/auth/login")
-    public String loginPage() {
-        return "login";
+    @PostMapping("/access")
+    public ResponseEntity<ApiResponse<String>> sendAccessToken(@CookieValue("refreshToken") String refreshToken) {
+        try {
+            Long userId = jwtTokenProvider.getUserIdFromRefreshToken(refreshToken);
+            String newAccessToken = jwtTokenProvider.refreshAccessToken(refreshToken, userId);
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Authorization", "Bearer " + newAccessToken);
+
+            return ResponseEntity.ok()
+                    .headers(headers)
+                    .body(ApiResponse.success(newAccessToken));
+            } catch (Exception e) {
+            log.error("token update failed: {}", e.getMessage());
+            throw new SecurityTokenException(ErrorCode.INTERNAL_TOKEN_SERVER_ERROR);
+        }
     }
 
-
-    @GetMapping("/home")
-    public String home(Model model) {
-        String name = userService.getCurrentUserName();
-        model.addAttribute("name", name);
-        return "home";
-    }
-
-    @PostMapping("/refresh")
+    @PostMapping("/reissue")
     public ResponseEntity<ApiResponse<String>> refreshAccessToken(@CookieValue("refreshToken") String refreshToken) {
         try {
             Long userId = jwtTokenProvider.getUserIdFromRefreshToken(refreshToken);
@@ -47,8 +48,8 @@ public class AuthController {
 
             return ResponseEntity.ok()
                     .headers(headers)
-                    .body(ApiResponse.success(newAccessToken)); // ApiResponse 형식의 성공 응답 반환
-            } catch (Exception e) {
+                    .body(ApiResponse.success(newAccessToken));
+        } catch (Exception e) {
             log.error("token update failed: {}", e.getMessage());
             throw new SecurityTokenException(ErrorCode.INTERNAL_TOKEN_SERVER_ERROR);
         }
