8c7e20d6fa7d4bd9d1ae10759e8f696a8e6fccc1
alswp006 <alswp006@github.com>
2024-11-11 17:09:47 +0900

    [Update] 일일 미션 반환 로직 수정

---
+++ src/main/java/com/devita/domain/mission/dto/client/DailyMissionResDTO.java
@@ -0,0 +1,9 @@
+package com.devita.domain.mission.dto.client;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class DailyMissionResDTO {
+    private Long missionId;
+    private String missionTitle;
+}
