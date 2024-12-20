8a8ef69bf83014425419e98da77cf80cfbc86ab0
alswp006 <alswp006@gmail.com>
2024-12-20 14:24:48 +0900

    [Update] 미션 완료 시 AI 서버에 넘겨주는 데이터 명 변경

--- src/main/java/com/devita/domain/todo/dto/TodoAIReqDTO.java
+++ src/main/java/com/devita/domain/todo/dto/TodoAIReqDTO.java
@@ -5,7 +5,7 @@ import java.time.LocalDate;
 public record TodoAIReqDTO(
         String title, //Java 공부하기
         LocalDate date, // 2024-12-02
-        String type, // 일일 미션, 자율 미션
+        String missionType, // 일일 미션, 자율 미션
         String missionCategory, // Java, Python
         Long userId // 1
 ) {}
